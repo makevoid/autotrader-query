3d27555ad1382adb13f8ed60a9ad217e2a2273b8
makevoid <makevoid@gmail.com>
2019-07-12 06:33:58 +0100

    I overdid it?

--- lib/get.rb
+++ lib/get.rb
@@ -7,24 +7,38 @@ class Get
 
   def get
     puts "URL: #{url}"
-    # body = get_net
-    body = get_mechanize
+    # body = get_mechanize
+    body = get_net
     JSON.parse body
   end
 
-  def get_net
-    uri  = URI url
-    resp = Net::HTTP.get_response uri
-    resp.body
+  # net-http get w/ user agent header
+
+  USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X x.y; rv:42.0) Gecko/20100101 Firefox/42.0"
+
+  def request(uri:)
+    req = Net::HTTP::Get.new uri
+    req['User-Agent'] = USER_AGENT
+    req['Accept'] = "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8"
+    req
   end
 
   def get_net
-    uri  = URI url
-    resp = Net::HTTP.get_response uri
-    resp.body
+    uri = URI url
+    req = -> (http) { http.request request(uri: uri) }
+    res = Net::HTTP.start(uri.hostname, uri.port){ |http| req.(http) }
+    res.body
   end
-  Mozilla/5.0 (Android 4.4; Mobile; rv:41.0) Gecko/41.0 Firefox/41.0
 
+  # or with get_response()
+
+  # def get_net_orig
+  #   uri  = URI url
+  #   resp = Net::HTTP.get_response uri
+  #   resp.body
+  # end
+
+  # or with mechanize ("magical" user agents + libxml2/nokogiri dom parsing support)
 
   # Agent = Mechanize.new
   # Agent.user_agent = "Mac Firefox" # lol
@@ -37,4 +51,5 @@ class Get
   def self.g(url)
     new(url).get
   end
+
 end
