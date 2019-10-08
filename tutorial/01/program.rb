4669453146cad36544a441234a6faff3c346d3ca
Francesco 'makevoid' Canessa <makevoid@gmail.com>
2019-10-08 01:51:14 +0100

    commit

--- tutorial/01/program.rb
+++ tutorial/01/program.rb
@@ -2,13 +2,84 @@
 # install ruby on windows https://rubyinstaller.org
 # if you're on linux, you probably know how to `apt install ruby` on your favourite distro
 
+
+# we're going to build a script to fetch
+
 # 1.
 
-# gem i nokogiri
+$ gem i nokogiri
 
 # a bit slow to install (c deps) but a very good api for html parsing
 
 # ....
+
+# quick demo of ruby and nokogiri before we start
+
+# open the terminal (in your Applications, Utilities directory, Terminal app)
+
+# open IRB
+
+require 'nokogiri'
+
+# this will tell you that nokogiri is loaded
+
+# nokogiri requires the `Nokogiri` class
+
+irb(main):002:0> Nokogiri
+=> Nokogiri
+
+
+#
+
+Nokogiri::HTML("<p></p>")
+
+
+#
+
+Nokogiri::HTML("<p>test</p>")
+
+#
+
+Nokogiri::HTML("<p>test</p>")
+
+#
+
+Nokogiri::HTML("<p>test</p>").inner_text
+
+#
+
+# -----------------
+
+irb(main):006:0> require 'net/http'
+=> true
+
+# ------
+
+irb(main):007:0> require 'json'
+=> true
+
+# ------
+
+irb(main):008:0> Net
+=> Net
+
+# ------
+
+irb(main):009:0> Net::HTTP
+=> Net::HTTP
+
+# ------
+
+irb(main):010:0> URI
+=> URI
+
+# ------
+
+irb(main):011:0> JSON
+=> JSON
+
+# ------
+
 
 
 # 2.
